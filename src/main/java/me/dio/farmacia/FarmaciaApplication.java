31086053427e0f17de37cdf34998ffac4b6b2ee2
Pucapuka <traderstrap1@gmail.com>
2024-07-14 19:33:55 -0300

    "Bugfix CORS"

--- src/main/java/me/dio/farmacia/FarmaciaApplication.java
+++ src/main/java/me/dio/farmacia/FarmaciaApplication.java
@@ -1,9 +1,12 @@
 package me.dio.farmacia;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@OpenAPIDefinition(servers= { @Server(url = "/", "Default Server URL")})
+@SpringBootApplication/
 public class FarmaciaApplication {
 
 	public static void main(String[] args) {
